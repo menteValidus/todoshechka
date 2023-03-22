78d69520bf3134e8601f74ee3cfb6fca8465dde1
Denis Cherny <39064926+menteValidus@users.noreply.github.com>
2023-03-22 19:21:46 +0400

    Create xcode project

---
+++ todoshechka/todoshechka/todoshechkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoshechkaApp.swift
+//  todoshechka
+//
+//  Created by Orlando Memando on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoshechkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
