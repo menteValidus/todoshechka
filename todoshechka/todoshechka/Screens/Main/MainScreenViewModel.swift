8bb397d14baa1a0ad254b711fc4a6a4f1df9afab
Denis Cherny <39064926+menteValidus@users.noreply.github.com>
2023-03-24 17:39:55 +0400

    Add VM and tests

---
+++ todoshechka/todoshechka/Screens/Main/MainScreenViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  Created on 24.03.2023.
+//
+
+import Foundation
+
+final class MainScreenViewModel: ObservableObject {
+    
+}
---
+++ todoshechka/todoshechkaTests/Screens/Main/MainScreenViewModelTests.swift
@@ -0,0 +1,11 @@
+//
+//  Created on 24.03.2023.
+//
+
+import XCTest
+
+final class MainScreenViewModelTests: XCTestCase {
+    func testInit() throws {
+        
+    }
+}
